2360412b9546a87c95c2e3692f3de095e14b1a88
github@mcitpnetworks.com <github@mcitpnetworks.com>
2021-11-13 13:58:06 -0500

    Update ADFSAUTH

--- HybridIdentity-ADFSwithSQL-WAP-ADConnect-Multi-Site/DSC/ADFSAUTH.ps1
+++ HybridIdentity-ADFSwithSQL-WAP-ADConnect-Multi-Site/DSC/ADFSAUTH.ps1
@@ -52,11 +52,11 @@
                     $uris = @("https://owa$using:ExchangeVersion.$using:ExternalDomainName/owa/","https://owa$using:ExchangeVersion.$using:ExternalDomainName/ecp/")
 
                     Set-OrganizationConfig -AdfsIssuer "https://adfs.$using:ExternalDomainName/adfs/ls/" -AdfsAudienceUris $uris -AdfsSignCertificateThumbprint $ADFSThumbprint
-
-                    # ADFS on FORMS off
-                    Set-OwaVirtualDirectory –Identity "$using:computerName\owa (Default Web Site)" -AdfsAuthentication $true -BasicAuthentication $false -DigestAuthentication $false -FormsAuthentication $false -WindowsAuthentication $false
-                    Set-EcpVirtualDirectory -Identity "$using:computerName\ecp (Default Web Site)" -AdfsAuthentication $true -BasicAuthentication $false -DigestAuthentication $false -FormsAuthentication $false -WindowsAuthentication $false
                 }
+
+                # ADFS on FORMS off
+                Set-OwaVirtualDirectory –Identity "$using:computerName\owa (Default Web Site)" -AdfsAuthentication $true -BasicAuthentication $false -DigestAuthentication $false -FormsAuthentication $false -WindowsAuthentication $false
+                Set-EcpVirtualDirectory -Identity "$using:computerName\ecp (Default Web Site)" -AdfsAuthentication $true -BasicAuthentication $false -DigestAuthentication $false -FormsAuthentication $false -WindowsAuthentication $false
             }
             GetScript =  { @{} }
             TestScript = { $false}
